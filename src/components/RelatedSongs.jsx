8e542fd366eb895a6e9135f12fea5142f1a33a6c
MinjiPark <pun.p0107@gmail.com>
2022-10-19 18:21:58 +0700

    Core

--- src/components/RelatedSongs.jsx
+++ src/components/RelatedSongs.jsx
@@ -1,10 +1,27 @@
 import React from "react"
-import { Songbar } from "./SongBar"
+import Songbar from "./SongBar"
 
-const RelatedSongs = () => 
+const RelatedSongs = ({ data, isPlaying, activeSong, handlePauseClick, handlePlayClick, artistId }) => 
 {
   return (
-    <div>RelatedSongs</div>
+    <div className= "flex flex-col">
+      <h1 className= "font-bold text-3xl text-white">Related Songs:</h1>
+
+      <div className= "mt-6 w-full flex flex-col">
+        { data?.map((song, i) => (
+          <Songbar 
+            key= { `${ song.key }-${ artistId }` }
+            song= { song }
+            i= { i }
+            artistId= { artistId }
+            isPlaying= { isPlaying }
+            activeSong= { activeSong }
+            handlePauseClick= { handlePauseClick }
+            handlePlayClick= { handlePlayClick }
+          />
+        )) }
+      </div>
+    </div>
   )
 }
 
--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -1,4 +1,4 @@
-import { createApi, fetchBaseQuery, retry } from "@reduxjs/toolkit/query/react"
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react"
 
 export const shazamCoreApi = createApi({ 
   reducerPath: "shazamCoreApi",
@@ -13,12 +13,20 @@ export const shazamCoreApi = createApi({
   endpoints: (builder) => ({
     getTopCharts: builder.query({ query: () => "/charts/world" }),
     getSongDetails: builder.query({ query: ({ songid }) => `/tracks/details?track_id=${ songid }` }),
-    getSongRelated: builder.query({ query: ({ songid }) =>  `/tracks/related?track_id=${ songid }`})
+    getSongRelated: builder.query({ query: ({ songid }) =>  `/tracks/related?track_id=${ songid }`}),
+    getArtistDetails: builder.query({ query: ({ artistId }) => `/artists/details?artist_id=${ artistId }` }),
+    getSongsByCountry: builder.query({ query: ({ countryCode }) => `/charts/country?country_code=${ countryCode }` }),
+    getSongsByGenre: builder.query({ query: (genre) => `/charts/genre-world?genre_code=${ genre }` }),
+    getSongsBySearch: builder.query({ query: (searchTerm) => `/search/multi?search_type=SONGS_ARTISTS&query=${ searchTerm }` })
   })
 })
 
 export const {
   useGetTopChartsQuery, 
   useGetSongDetailsQuery, 
-  useGetSongRelatedQuery
+  useGetSongRelatedQuery,
+  useGetArtistDetailsQuery,
+  useGetSongsByCountryQuery,
+  useGetSongsByGenreQuery,
+  useGetSongsBySearchQuery
 } = shazamCoreApi
